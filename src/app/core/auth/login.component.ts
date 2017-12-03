d2f4126ea2b384a74cda78120d96b36944058046
Nikolay Gelemerov <nikolaygelemerov@gmail.com>
2017-12-03 23:22:36 +0200

    add new users in database

--- src/app/core/auth/login.component.ts
+++ src/app/core/auth/login.component.ts
@@ -3,6 +3,7 @@ import { IgxDialog } from 'igniteui-js-blocks/main';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 import { AngularFirestore } from 'angularfire2/firestore';
+import {LawyerModel} from '../../lawyers/LawyerModel';
 
 @Component({
   selector: 'app-login-component',
@@ -15,6 +16,8 @@ export class LoginComponent {
   email;
   password;
   lawyersCollection;
+
+  model: LawyerModel;
 
   constructor(public afAuth: AngularFireAuth, private db: AngularFirestore) {
     this.lawyersCollection = db.collection('users');
@@ -33,12 +36,30 @@ export class LoginComponent {
 
   googleLogin() {
     this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then((data: firebase.User) => {
+      console.log('data: ', data);
       this.loginDialog.close();
       this.addToUsersIfDontExist(data);
     });
   }
 
-  addToUsersIfDontExist({ uid, displayName, email, phoneNumber, photoURL }) {
-    this.lawyersCollection.add({ uid, displayName, email, phoneNumber, photoURL, wins: 0, loses: 0 });
+  addToUsersIfDontExist(data): void {
+    this.model = {
+      photoURL: data.user.photoURL,
+      displayName: data.user.displayName,
+      email: data.user.email,
+      phoneNumber: data.user.phoneNumber,
+      wins: 0,
+      loses: 0
+    };
+
+    this.db.collection('users').doc(data.user.uid).set(this.model)
+      .then(result => {
+        console.log('result: ', result);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+
+    this.lawyersCollection.add(this.model); // not needed
   }
 }
